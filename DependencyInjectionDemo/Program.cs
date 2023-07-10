f417d89007c930df6fea2904546f04e1166ac3ae
howdykimchi <howdykimchi@gmail.com>
2023-07-09 20:43:05 -0400

    install Serilog.AspNetCore, overrides the default logger with Serilog instead

--- DependencyInjectionDemo/Program.cs
+++ DependencyInjectionDemo/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using DependencyInjectionDemo.Logic;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,18 @@ builder.Services.AddServerSideBlazor();
 //builder.Services.AddSingleton<DemoLogic>(); //singleton
 //builder.Services.AddScoped<IDemoLogic, DemoLogic>(); //each instance have their singleton(localize), per connection(per user)
 //builder.Services.AddTransient<IDemoLogic, BetterDemoLogic>(); //with one change now change entire application
-builder.Services.AddTransient<IDemoLogic, DemoLogic>(); //how easy it is to change dependencies around
+//builder.Services.AddTransient<IDemoLogic, DemoLogic>(); //how easy it is to change dependencies around
+//builder.Services.AddTransient<IDemoLogic, DemoLogic>();
+//builder.Services.AddTransient<IDemoLogic, BetterDemoLogic>(); 
+//if put two different class instance into under the same interface in dependncy injection
+//last one because the last one added
+builder.Services.AddTransient<IDemoLogic, DemoLogic>();
+
+//this overrides the default logger with Serilog instead.
+builder.Host.UseSerilog((context, config) =>
+{
+    config.WriteTo.Console();
+});
 
 
 var app = builder.Build();
